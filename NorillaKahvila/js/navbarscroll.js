63f0fc011a49b60d65c73dfa0459a5e86922fbb5
My Lan Phuong <mylang.phuong@gmail.com>
2017-08-21 12:29:43 +0300

    Add files via upload

---
+++ NorillaKahvila/js/navbarscroll.js
@@ -0,0 +1,13 @@
+$(document).scroll(function(e){
+    var scrollTop = $(document).scrollTop();
+    if(scrollTop > 0){
+        console.log(scrollTop);
+        $('.navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top');
+    } else {
+        $('.navbar').removeClass('navbar-fixed-top').addClass('navbar-static-top');
+    }
+});
+
+$("#nav-button1").click(function(){
+    //alert("The paragraph was clicked.");
+});
