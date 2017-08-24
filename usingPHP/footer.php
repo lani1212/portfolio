8359f6f86983ac3eeace9106c298fb5d761e96d7
My Lan Phuong <mylang.phuong@gmail.com>
2017-08-24 21:56:36 +0300

    Add files via upload

---
+++ usingPHP/footer.php
@@ -0,0 +1,16 @@
+<!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="https://github.com/MyLan1212">Github</a></li>
+				<li><a href="https://www.linkedin.com/in/my-lan-phuong-a33675129/">Linkedin</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
+
+		</div>
+	
+	</div>
---
+++ usingPHP/thanks.php
@@ -0,0 +1,16 @@
+<?php
+
+//To Do: Send Email
+
+$pageTitle = "Thank You";
+$section = null;
+
+include("inc/header.php"); ?>
+
+
+<div class="section page"> 
+	<h1>Thank You</h1>
+	<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly! </p>
+</div>
+
+<?php include("inc/footer.php"); ?>
